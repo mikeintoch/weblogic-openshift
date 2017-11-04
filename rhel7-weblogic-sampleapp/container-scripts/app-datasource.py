fa4496f6108016d7f9c6a1679bc3a480663588fb
David Gamboa <idavistro@gmail.com>
2017-11-03 20:41:37 -0500

    Update app-datasource.py

--- rhel7-weblogic-sampleapp/container-scripts/app-datasource.py
+++ rhel7-weblogic-sampleapp/container-scripts/app-datasource.py
@@ -4,15 +4,25 @@ import os
 domainhome = os.environ.get('DOMAIN_HOME', '/u01/oracle/user_projects/domains/base_domain')
 
 # Definition DataSource Properties
-dsName            = "ds_atm_movil"
-dsDatabaseName    = "test"
-datasourceTarget  = "AdminServer"
-dsJNDIName        = "jdbc/atm_movil"
-dsDriverName      = "com.mysql.jdbc.Driver"
-dsURL             = "jdbc:mysql://192.168.42.1:3306/test"
-dsUserName        = "openshiftUser"
-dsPassword        = "R3dh4t1!"
-dsTestQuery       = "SQL SELECT 1"
+# dsName            = "ds_atm_movil"
+# dsDatabaseName    = "test"
+# datasourceTarget  = "AdminServer"
+# dsJNDIName        = "jdbc/atm_movil"
+# dsDriverName      = "com.mysql.jdbc.Driver"
+# dsURL             = "jdbc:mysql://192.168.42.1:3306/test"
+# dsUserName        = "openshiftUser"
+# dsPassword        = "R3dh4t1!"
+# dsTestQuery       = "SQL SELECT 1"
+
+dsName            = os.environ.get('dsName')
+dsDatabaseName    = os.environ.get('dsDatabaseName')
+datasourceTarget  = os.environ.get('datasourceTarget')
+dsJNDIName        = os.environ.get('dsJNDIName')
+dsDriverName      = os.environ.get('dsDriverName')
+dsURL             = os.environ.get('dsURL')
+dsUserName        = os.environ.get('dsUserName')
+dsPassword        = os.environ.get('dsPassword')
+dsTestQuery       = os.environ.get('dsTestQuery')
 
 # Read Domain in Offline Mode
 # ===========================
@@ -50,14 +60,14 @@ cd('Properties/NO_NAME_0')
 create('user','Property')
 cd('Property')
 cd('user')
-set('Value', 'openshiftUser')
+set('Value', dsUserName)
 
  
 # Create JDBCConnectionPoolParams
 cd('/JDBCSystemResource/' + dsName +'/JdbcResource/' + dsName)
 create('myJdbcConnectionPoolParams','JDBCConnectionPoolParams')
 cd('JDBCConnectionPoolParams/NO_NAME_0')
-set('TestTableName','SQL SELECT 1')
+set('TestTableName',dsTestQuery)
 
 updateDomain()
 closeDomain()
